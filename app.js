96e80da32b63e7a29a2e41dc653287978966dd31
NatanPivetta <natanpivetta@github.com>
2022-11-29 16:12:37 -0300

    Estilização

--- app.js
+++ app.js
@@ -11,47 +11,45 @@ const options = {
     }
 }
 
-    function callback_BTC(err, res, body) {
+    const callback_BTC = function (err, res, body) {
     let json = JSON.parse(body);
-    const bid = json.BTCBRL['bid'];
+    const cotacao = json.BTCBRL['bid'];
     const code = json.BTCBRL['code'];
     const codein = json.BTCBRL['codein'];
-    const pctChange = json.BTCBRL['pctChange'];
-    const BTC_BRL =json.BTCBRL;
-    console.log(BTC_BRL)
-    // console.log(code +"/"+ codein);
-    // console.log('R$ ' + cotacao);
-    // console.log(pctVar);
-    // console.log('');
+    const pctVar = json.BTCBRL['pctChange'];
+    //console.log(json.BTCBRL)
+    console.log(code +"/"+ codein);
+    console.log('R$ ' + cotacao);
+    console.log(pctVar);
+    console.log('');
 
 }
 
-    function callback_ETH (err, res, body) {
+const callback_ETH = function (err, res, body) {
     let json = JSON.parse(body);
-    const bid = json.ETHBRL['bid'];
+    const cotacao = json.ETHBRL['bid'];
     const code = json.ETHBRL['code'];
     const codein = json.ETHBRL['codein'];
-    const pctChange = json.ETHBRL['pctChange'];
-    
-    // console.log(code +"/"+ codein);
-    // console.log('R$ ' + cotacao);
-    // console.log(pctVar);
-    
+    const pctVar = json.ETHBRL['pctChange'];
+    // console.log(json.ETHBRL)
+    console.log(code +"/"+ codein);
+    console.log('R$ ' + cotacao);
+    console.log(pctVar);
+
 }
 
+request(options, callback_BTC);
+request(options, callback_ETH);
+
+//document.getElementById("btc").innerHTML = `${callback_BTC.code} / ${callback_BTC.codein}`;
+//document.getElementById("preco").innerHTML = `${callback_BTC.cotacao}`;
 
 
-let btc_json = request(options, callback_BTC);
-let eth_json = request(options, callback_ETH);
+//const myInterval = setInterval(clock, 1000);
 
-// document.getElementById("btc").innerHTML = `${callback_BTC.code} / ${callback_BTC.codein}`;
-// document.getElementById("preco").innerHTML = `${callback_BTC.cotacao}`;
+//function clock() {
+  //  document.getElementById("btc").innerHTML = `${callback_BTC.code} / ${callback_BTC.codein}`;
+    //document.getElementById("preco_btc").innerHTML = `${callback_BTC.cotacao}`;
 
+//}
 
-// const myInterval = setInterval(clock, 1000);
-
-// function clock() {
-//     document.getElementById("btc").innerHTML = `${callback_BTC.code} / ${callback_BTC.codein}`;
-//     document.getElementById("preco_btc").innerHTML = `${callback_BTC.cotacao}`;
-
-// }
